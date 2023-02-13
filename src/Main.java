ef8cef235fef917b208c40b999fa088241848474
МАРК <markpan07@mail.ru>
2023-02-13 14:16:56 +0300

    Middle level is done!

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         employees[0] = new Employee("Perov", "Ivan", "Alekseevich", 1, 50_000);
-        employees[1] = new Employee("Pankov", "Mark", "Dmitrievich", 1, 70_000);
-        employees[2] = new Employee("Gres", "Roman", "Antonovich", 3, 100_000);
+        employees[1] = new Employee("Pankov", "Mark", "Dmitrievich", 1, 270_000);
+        employees[2] = new Employee("Gres", "Roman", "Antonovich", 3, 300_000);
         employees[3] = new Employee("Saveliev", "Yuriy", "Afanasievich", 5, 190_000);
         employees[4] = new Employee("Pavlov", "Stepan", "Aleksandrovich", 4, 120_000);
         employees[5] = new Employee("Grishin", "Dmitriy", "Stepanovich", 4, 105_000);
@@ -17,12 +17,13 @@ public class Main {
 
         employees[1].setSalary(150000);
         employees[1].setDepartment(3);
-        System.out.println(employees.length);
+
+        //Easy level
 
         System.out.println("Сумма затрат на зп сотрудникам: " + countEmployeeSalarySum() + "\n");
         System.out.println("Всего сотрудников в компании: " + getEmployeeQuantity() + "\n");
 
-        System.out.println("Средняя зп сотрудников: " + meanSalary() + "\n");
+        System.out.println("Средняя зп сотрудников: " + countMeanSalary() + "\n");
         System.out.println("Сотрудник с минимальной зп: \n" + findEmployeeWithMinimalSalary() + "\n");
         System.out.println("Сотрудник с максимальной зп: \n" + findEmployeeWithMaximalSalary() + "\n");
 
@@ -30,33 +31,95 @@ public class Main {
         printArray(getEmployeeFullNameList());
         System.out.println("___________________________________");
 
-        //increaseSalaryForAllEmployees(10);
-        for (int i = 0; i < employees.length && (employees[i] != null); i++) {
-            System.out.println(employees[i]);
-        }
-
-        System.out.println(findEmployeeWithMinimalSalary(2));
-        System.out.println(findEmployeeWithMaximalSalary(5));
-        System.out.println(countSumOfSalaryInDepartment(2));
-        System.out.println(countMeanSalaryInDepartment(2));
+        System.out.println("Информация по 2му департаменту:" + "\n");
+        System.out.println("Сотрудник с минимальной зп: \n" + findEmployeeWithMinimalSalary(2)+ "\n");
+        System.out.println("Сотрудник с максимальной зп: \n" + findEmployeeWithMaximalSalary(5) + "\n");
+        System.out.println("Сотрудники департамента: ");
+        printArray(getEmployeeInfoListInDepartment(2));
+        System.out.println("Сумма зарплат в департаменте: " + countSumOfSalaryInDepartment(2) +  "\n");
+        System.out.println("Срденяя ЗП в департаменте: " + countMeanSalaryInDepartment(2) + "\n");
+        System.out.println("ЗП после индексации на 10%% у сотрудников в департаменте");
         increaseSalaryForEmployeesInDepartment(2, 10);
-        System.out.println(countSumOfSalaryInDepartment(2));
+        printArray(getEmployeeInfoListInDepartment(2));
+        System.out.println("___________________________________");
 
+
+        System.out.println("Сотрудники в  с ЗП меньше 100 000 рублей: " + getEmployeeQuantitySalaryLess(100000));
+        printArray(getEmployeeListWithSalaryLess(100000));
+        System.out.println();
+        System.out.println("Сотрудники с ЗП больше 100 000 рублей: " + getEmployeeQuantitySalaryMore(100000));
+        printArray(getEmployeeListWithSalaryMore(100000));
     }
 
 
-    public static int getEmployeeQuantity() {
-        return Employee.count;
+    // Custom helpful methods
+
+    public static void printArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 
-    public static int countNotNullElementsInArray() {
-        int i = 0;
+    public static int getEmployeeQuantitySalaryLess(int salary) {
+        int count = 0;
         for (Employee empl : employees) {
-            if (employees[i] != null) {
-                i++;
+            if ((empl != null) && (empl.getSalary() < salary)) {
+                count++;
             }
         }
-        return i;
+        return count;
+    }
+
+    public static int getEmployeeQuantitySalaryMore(int salary) {
+        int count = 0;
+        for (Employee empl : employees) {
+            if ((empl != null) && (empl.getSalary() >= salary)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String[] getEmployeeListWithSalaryLess(int salary) {
+        String[] employeeList = new String[getEmployeeQuantitySalaryLess(salary)];
+        int count = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if ((employees[i] != null) && (employees[i].getSalary() < salary)) {
+                employeeList[count] = "ФИО: " + employees[i].getLastName() + " "
+                        + employees[i].getFirstName() + " " + employees[i].getThirdName() + ", ЗП: "
+                        + employees[i].getSalary() + ", ID: " + employees[i].getId();
+                count++;
+            }
+        }
+        return employeeList;
+
+    }
+
+    public static String[] getEmployeeListWithSalaryMore(int salary) {
+        String[] employeeList = new String[getEmployeeQuantitySalaryMore(salary)];
+        int count = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if ((employees[i] != null) && (employees[i].getSalary() >= salary)) {
+                employeeList[count] = "ФИО: " + employees[i].getLastName() + " "
+                        + employees[i].getFirstName() + " " + employees[i].getThirdName() + ", ЗП: "
+                        + employees[i].getSalary() + ", ID: " + employees[i].getId();
+                count++;
+            }
+        }
+        return employeeList;
+
+    }
+
+    // Methods for all employees
+
+    public static int getEmployeeQuantity() {
+        int count = 0;
+        for (Employee empl : employees) {
+            if (empl != null) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static double countEmployeeSalarySum() {
@@ -98,12 +161,12 @@ public class Main {
         return employees[position];
     }
 
-    public static double meanSalary() {
-        return countEmployeeSalarySum() / countNotNullElementsInArray();
+    public static double countMeanSalary() {
+        return countEmployeeSalarySum() / getEmployeeQuantity();
     }
 
     public static String[] getEmployeeFullNameList() {
-        String[] fullNameList = new String[countNotNullElementsInArray()];
+        String[] fullNameList = new String[getEmployeeQuantity()];
         for (int i = 0; i < employees.length; i++) {
             if (employees[i] != null) {
                 fullNameList[i] = employees[i].getId() + " " + employees[i].getLastName() + " "
@@ -113,14 +176,6 @@ public class Main {
         return fullNameList;
     }
 
-    public static void printArray(String[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-    }
-
-    //Middle level of project
-
     public static void increaseSalaryForAllEmployees(int percent) {
         for (int i = 0; i < employees.length; i++) {
             if (employees[i] != null) {
@@ -128,6 +183,33 @@ public class Main {
                 employees[i].setSalary(newSalary);
             }
         }
+    }
+
+
+    //Methods for department
+    public static String[] getEmployeeInfoListInDepartment(int department) {
+        String[] fullNameList = new String[getEmployeeQuantityInDepartment(department)];
+        int count = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if ((employees[i] != null) && (employees[i].getDepartment() == department)) {
+                fullNameList[count] = "ФИО: " + employees[i].getLastName() + " "
+                        + employees[i].getFirstName() + " " + employees[i].getThirdName() + ", ЗП: "
+                        + employees[i].getSalary() + ", ID: " + employees[i].getId();
+                count++;
+            }
+        }
+        return fullNameList;
+
+    }
+
+    public static int getEmployeeQuantityInDepartment(int department) {
+        int count = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].getDepartment() == department) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static Employee findEmployeeWithMinimalSalary(int department) {
